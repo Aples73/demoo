13cffc7def38f9bcdb39fa777d932ea6b124064f
Aples73 <aplesbagibagi@gmail.com>
2023-12-27 22:33:51 -0800

    first commit

---
+++ app/Models/Atk_model.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Atk_model extends CI_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function get_all_atk()
+    {
+        return $this->db->get('atk')->result_array();
+    }
+
+    public function insert_atk($data)
+    {
+        $this->db->insert('atk', $data);
+    }
+}
